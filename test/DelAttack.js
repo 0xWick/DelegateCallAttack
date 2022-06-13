3ebfa090ae91ced8b80b273d2a32080c8eb472b0
Wick.py <wickprof@gmail.com>
2022-06-13 20:14:55 +0500

    first commit

---
+++ test/DelAttack.js
@@ -0,0 +1,34 @@
+const { inputToConfig } = require("@ethereum-waffle/compiler");
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { ethers, waffle } = require("hardhat");
+
+describe("Attack", function () {
+    it("Should change the owner of the Good Contract", async function () {
+        // Deploy the helper contract
+        const helperContract = await ethers.getContractFactory("Helper");
+        const _helperContract = await helperContract.deploy();
+        await _helperContract.deployed();
+        console.log("Helper Contract's Address:", _helperContract.address);
+
+        // Deploy the good Contract
+        const goodContract = await ethers.getContractFactory("Good");
+        const _goodContract = await goodContract.deploy(_helperContract.address);
+        await _goodContract.deployed();
+        console.log("Good Contract's Address:", _goodContract.address);
+
+        // Deploy the Attack contract
+        const attackContract = await ethers.getContractFactory("Attack");
+        const _attackContract = await attackContract.deploy(_goodContract.address);
+        await _attackContract.deployed();
+        console.log("Attack Contract's Address:", _attackContract.address);
+
+        // Now lets attack the good Contract
+
+        // Start the attack
+        let tx = await _attackContract.attack();
+        await tx.wait();
+
+        expect(await _goodContract.owner()).to.equal(_attackContract.address);
+    });
+});
